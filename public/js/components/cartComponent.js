cf7e7b21bec91421f270e35fc69709cc2295fb37
kramargit <89024802+kramargit@users.noreply.github.com>
2021-12-08 00:19:59 +0700

    Add files via upload

---
+++ public/js/components/cartComponent.js
@@ -0,0 +1,85 @@
+Vue.component('cart', {
+    data() {
+        return {
+            cartItems: [],
+            discount: 0
+        }
+    },
+    mounted() {
+        this.$parent.getJson(`/api/cart`)
+            .then(data => {
+                for (let item of data.contents) {
+                    this.$data.cartItems.push(item);
+                }
+            })
+
+    },
+    methods: {
+        sumProducts() {
+            let sum = 0;
+            for (let item of this.cartItems) {
+                sum += (item.quantity * item.price);
+            }
+            return sum;
+        },
+        remove(item) {
+            let change = this.cartItems.find(el => el.id_product === item.id_product);
+            this.$parent.deleteJson(`/api/cart`, item)
+                .then(data => {
+                    if (data.result === 1) {
+                        this.cartItems = data.contents
+                    }
+                })
+        }
+    },
+    template: `<div class="cart_wrap">
+                    
+                    <div class="cart_left">
+                        <h1 v-if="cartItems.length == 0">Cart is empty!</h1>
+                        <cart-item v-for="item of cartItems" :key="item.id_product" :product="item" @delete-product="remove">
+                        </cart-item>
+                        <div class="cart_left_btn_box">
+                                <button class="cart_left_btn clear">clear shopping cart</button>
+                                <button class="cart_left_btn continue">continue shopping</button>
+                        </div>
+                    </div>
+                    <div class="cart_right">
+                        <span class="cart_right_title">shipping adress</span>
+                        <form class="cart_right_form" action="#" method="POST">
+                            <input class="cart_right_form_country cart_right_form_input" type="text"
+                                placeholder="Bangladesh">
+                            <input class="cart_right_form_state cart_right_form_input" type="text"
+                                placeholder="State">
+                            <input class="cart_right_form_postcode cart_right_form_input" type="text"
+                                placeholder="Postcode / Zip">
+                            <button class="cart_right_form_btn">get a quote</button>
+                            <div class="cart_right_form_total_info">
+                                <span class="cart_right_form_total_info_sub">sub total <span> {{ sumProducts() }}</span></span>
+                                <span class="cart_right_form_total_info_grand">grand total <span>{{ sumProducts() - this.discount }}</span></span>
+                                <hr class="cart_right_form_total_info_hr" />
+                                <input class="cart_right_form_total_info_submit" type="submit"
+                                    value="proceed to checkout">
+                            </div>
+                        </form>
+                    </div>
+                </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['product'],
+    template: `<article class="cart_left_product">
+                    <div class="cart_left_product_img">
+                        <img :src="product.img_product" alt="product.product_name">
+                    </div>
+                    <div class="cart_left_product_desc">
+                        <p class="cart_left_product_title cart_name">{{ product.product_name }}</p>
+                        <p class="cart_left_product_title cart_type">{{ product.product_type }}</p>
+                        <p class="cart_left_product_info cart_price">Price: <span>{{ product.price }}</span></p>
+                        <p class="cart_left_product_info cart_color">Color: <span>{{ product.product_color }}</span></p>
+                        <p class="cart_left_product_info cart_size">Size: <span>{{ product.product_size }}</span></p>
+                        <p class="cart_left_product_info cart_quantity">Quantity: <input
+                                class="cart_quantity_value" type="text" :placeholder="product.quantity"></p>
+                    </div>
+                    <button class="cart_left_product_close" @click="$emit('delete-product', product)"></button>
+                </article>`
+});
---
+++ public/js/components/main.js
@@ -0,0 +1,55 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: ''
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => this.$refs.error.text = error);
+        },
+
+        postJson(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => this.$refs.error.text = error);
+        },
+
+        putJson(url, data) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => this.$refs.error.text = error);
+        },
+        deleteJson(url, data) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+            /*.catch(error => {
+                this.$refs.error.setError(error);
+            });*/
+        }
+    },
+
+    mounted() {
+
+    }
+
+});
